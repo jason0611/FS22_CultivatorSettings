f21ed2081f74b530dea98b2721ee1d7b4fa0dcdb
Martin Eller <martin@eller-essen.de>
2024-05-20 20:18:30 +0200

    Update cultivatorSettings.lua

--- cultivatorSettings.lua
+++ cultivatorSettings.lua
@@ -83,9 +83,9 @@ function CultivatorSettings:onLoad(savegame)
 	CultivatorSettings.isDedi = g_server ~= nil and g_currentMission.connectedToDedicatedServer
 	
 	-- Make Specialization easier accessible
-	self.spec_DeepCultivator = self["spec_"..CultivatorSettings.MOD_NAME..".CultivatorSettings"]
+	self.spec_CultivatorSettings = self["spec_"..CultivatorSettings.MOD_NAME..".CultivatorSettings"]
 	
-	local spec = self.spec_DeepCultivator
+	local spec = self.spec_CultivatorSettings
 	spec.dirtyFlag = self:getNextDirtyFlag()
 	
 	spec.mode = 1
@@ -95,7 +95,7 @@ end
 
 function CultivatorSettings:onPostLoad(savegame)
 	dbgprint("onPostLoad: "..self:getFullName(), 2)
-	local spec = self.spec_DeepCultivator
+	local spec = self.spec_CultivatorSettings
 	
 	-- Get DC configuration
 	spec.config = self.configurations["CultivatorSettings"] or 0
@@ -128,7 +128,7 @@ end
 
 function CultivatorSettings:saveToXMLFile(xmlFile, key, usedModNames)
 	dbgprint("saveToXMLFile", 2)
-	local spec = self.spec_DeepCultivator
+	local spec = self.spec_CultivatorSettings
 	spec.config = self.configurations["CultivatorSettings"] or 0
 	if spec.config > 0 then
 		xmlFile:setValue(key.."#config", spec.config)
@@ -142,21 +142,21 @@ end
 
 function CultivatorSettings:onReadStream(streamId, connection)
 	dbgprint("onReadStream", 3)
-	local spec = self.spec_DeepCultivator
+	local spec = self.spec_CultivatorSettings
 	spec.config = streamReadInt8(streamId, connection)
 	spec.mode = streamReadInt8(streamId, connection)
 end
 
 function CultivatorSettings:onWriteStream(streamId, connection)
 	dbgprint("onWriteStream", 3)
-	local spec = self.spec_DeepCultivator
+	local spec = self.spec_CultivatorSettings
 	streamWriteInt8(streamId, spec.config)
 	streamWriteInt8(streamId, spec.mode)
 end
 	
 function CultivatorSettings:onReadUpdateStream(streamId, timestamp, connection)
 	if not connection:getIsServer() then
-		local spec = self.spec_DeepCultivator
+		local spec = self.spec_CultivatorSettings
 		if streamReadBool(streamId) then
 			dbgprint("onReadUpdateStream: receiving data...", 4)
 			spec.config = streamReadInt8(streamId)
@@ -167,7 +167,7 @@ end
 
 function CultivatorSettings:onWriteUpdateStream(streamId, connection, dirtyMask)
 	if connection:getIsServer() then
-		local spec = self.spec_DeepCultivator
+		local spec = self.spec_CultivatorSettings
 		if streamWriteBool(streamId, bitAND(dirtyMask, spec.dirtyFlag) ~= 0) then
 			dbgprint("onWriteUpdateStream: sending data...", 4)
 			streamWriteInt8(streamId, spec.config)
@@ -181,7 +181,7 @@ end
 function CultivatorSettings:onRegisterActionEvents(isActiveForInput)
 	dbgprint("onRegisterActionEvents", 4)
 	if self.isClient then
-		local spec = self.spec_DeepCultivator
+		local spec = self.spec_CultivatorSettings
 		CultivatorSettings.actionEvents = {} 
 		if self:getIsActiveForInput(true) and spec ~= nil and spec.config == 4 then 
 			local prio = GS_PRIO_LOW
@@ -193,7 +193,7 @@ end
 
 function CultivatorSettings:TOGGLE(actionName, keyStatus, arg3, arg4, arg5)
 	dbgprint("TOGGLE", 4)
-	local spec = self.spec_DeepCultivator
+	local spec = self.spec_CultivatorSettings
 	dbgprint_r(spec, 4)
 	
 	spec.mode = spec.mode + 1
@@ -215,7 +215,7 @@ function CultivatorSettings:TOGGLE(actionName, keyStatus, arg3, arg4, arg5)
 end
 
 function CultivatorSettings:getPowerMultiplier(superfunc)
-	local spec = self.spec_DeepCultivator
+	local spec = self.spec_CultivatorSettings
 	local multiplier = 1
 	if spec.mode == 2 then multiplier = 0.5 end
 	if spec.mode == 3 then multiplier = 1.8 end
@@ -225,7 +225,7 @@ end
 -- change setting
 
 function CultivatorSettings:onUpdate(dt)
-	local spec = self.spec_DeepCultivator
+	local spec = self.spec_CultivatorSettings
 	local specCV = self.spec_cultivator
 	
 	if spec ~= nil and specCV ~= nil and spec.mode ~= spec.lastMode then
@@ -244,7 +244,7 @@ function CultivatorSettings:onUpdate(dt)
 end
 
 function CultivatorSettings:onDraw(dt)
-	local spec = self.spec_DeepCultivator
+	local spec = self.spec_CultivatorSettings
 	if spec ~= nil then 
 		if spec.mode == 2 then
 			g_currentMission:addExtraPrintText(g_i18n.modEnvironments[CultivatorSettings.MOD_NAME]:getText("shallowModeShort"))
